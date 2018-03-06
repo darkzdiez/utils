38a0c5ea6ed02d1d2cb2cd50b1efbaf2436017dc
Alfonzo Diez <alfonzodiez@gmail.com>
2018-03-06 11:57:12 -0400

    added helper sanitizeNumber

--- src/Support/helpers.php
+++ src/Support/helpers.php
@@ -12,4 +12,38 @@ if (! function_exists('dconfig')) {
         return new Hard\Utils\DConfig;
     }
 }
+if (! function_exists('dconfig')) {
+    /**
+     * Generate Title for table filter.
+     *
+     * @param  string  $display_name
+     * @param  string  $key_name
+     * @return string
+     */
+    function dconfig()
+    {
+        return new Hard\Utils\DConfig;
+    }
+}
+if (! function_exists('sanitizeNumber')) {
+    /**
+     * Generate Title for table filter.
+     *
+     * @param  string  $display_name
+     * @param  string  $key_name
+     * @return string
+     */
+    function sanitizeNumber($value)
+    {
+        $string = str_split($value);
+        $valid = '';
+        foreach ($string as $character) {
+            if(preg_match('/([0-9]|\.)/',$character)){
+                $valid .= $character;
+            }
+        }
+        return $valid;
+    }
+}
+
 ?>
