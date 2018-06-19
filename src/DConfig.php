c4d86548822de548744ba44b4dcf0a7c9e05048b
darkzdiez <alfonzodiez@gmail.com>
2018-06-19 10:30:01 -0400

    added new features

--- src/DConfig.php
+++ src/DConfig.php
@@ -3,14 +3,14 @@ namespace Hard\Utils;
 
 use Gate;
 use Illuminate\Support\Facades\Log;
-use Hard\Utils\Configuration;
+use App\EnvVar;
 
 class DConfig
 {
 
     public function get($config_key)
     {
-    	$config = Configuration::where('config_key', $config_key)->first();
+    	$config = EnvVar::where('config_key', $config_key)->first();
     	if ($config != null) {
     		return $config->config_value;
     	}
